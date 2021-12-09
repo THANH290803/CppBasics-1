9fde3f859d62c374382d1ea6bfb7ab95d31cac5f
hoangthanh <phucthanh290803@gmail.com>
2021-12-09 16:28:39 +0700

    Employee manager

--- 15-Mini-project/15-Mini-project.cpp
+++ 15-Mini-project/15-Mini-project.cpp
@@ -1,20 +1,147 @@
-// 15-Mini-project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+#include "Employee.h"
+#include "Programmer.h"
+#include "Tester.h"
+#include "Manager.h"
+#include "Utils.h"
 
-#include <iostream>
+void showMenu() {
+	cout << "-------------------Menu------------------------" << '\n';
+	cout << "1. Add employee" << '\n';
+	cout << "2. Edit employee" << '\n';
+	cout << "3. Remove employee" << '\n';
+	cout << "4. Sort employee by name" << '\n';
+	cout << "5. Show all employees" << '\n';
+	cout << "0. Exit" << '\n';
+	cout << "-----------------------------------------------" << '\n';
+	cout << "Please choose: ";
+}
+
+void showAll(vector<Employee*> list) {
+	for (auto e : list) {
+		e->show();
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//Employee* e{ new Employee() };
+
+	//e->input();
+
+	//e->show();
+
+	//Employee* p{ new Programmer() };
+	//Employee* t{ new Tester() };
+	//Employee* m{ new Manager() };
+	/*
+	vector<int> numbers{ 1, 2, 3 };
+	cout << numbers[0] << '\n';
+	cout << numbers.at(1) << '\n';
+
+	numbers.push_back(4);
+	cout << numbers.at(3);
+	*/
+	//vector<Employee*> list{};
+
+	//list.push_back(e);
+	//list.push_back(p);
+	//list.push_back(t);
+	//list.push_back(m);
+
+	//for (int i{ 0 }; i < list.size(); ++i) {
+	//	list.at(i)->input();
+	//}
+
+	//for (Employee* e : list) {
+	//for (auto e : list) {
+	//	e->show();
+	//}
+
+	vector<Employee*> list{};
+	ifstream fileIn{ "input.txt" };
+
+	// doc du lieu tu file
+	string line;
+	while (getline(fileIn, line)) {
+
+		auto vec = Utils::split(line, ",");
+
+		if (vec.at(0) == "1")
+		{
+			list.push_back(new Programmer{ stoi(vec.at(1)), vec.at(2) });
+		}
+		else if (vec.at(0) == "2")
+		{
+			list.push_back(new Tester{ stoi(vec.at(1)), vec.at(2) });
+		}
+		else if (vec.at(0) == "3")
+		{
+			list.push_back(new Manager{ stoi(vec.at(1)), vec.at(2) });
+		}
+	}
+	
+		//for (auto e : list) {
+		//	e->show();
+		//}
+	int choose;
+	bool exit{ false };
+
+	showMenu();
+	while (true)
+	{
+		cin >> choose;
+		switch (choose)
+		{
+		case 1:
+			cout << "Add employee" << '\n';
+			break;
+		case 2:
+			cout << "Edit employee" << '\n';
+			break;
+		case 3:
+			cout << "Remove employee" << '\n';
+			break;
+		case 4:
+			cout << "Sort by name" << '\n';
+			break;
+		case 5:
+			cout << "Show all employees" << '\n';
+			showAll(list);
+			break;
+		case 0:
+			cout << "Exit" << '\n';
+			exit = true;
+			break;
+		}
+		if (exit) {
+			break;
+		}
+		showMenu();
+	}
+		
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+/*
+Quản lý nhân viên 
+- Có đc danh sách nhân viên
+- Xem, sửa thông tin của nvien 
+- Thêm, xóa nhân viên 
+- Sắp xếp tìm kiếm nhân viên (tên, lương)
+*/
+
+/*
+- Nhân viên: Programming, Tester, Manager, ... => Base class Employee
+- List (array) -> vector<Employee *>
+- Logics: sort, search, ...
+
+- Giao diện; console, menu
+*/
+/*
+- Add employee:
+    + menu: chonj loại nv (pr, t, m)
+	+ if...=>new...=> list.push_back()
+*/
---
+++ 15-Mini-project/Tester.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Employee.h"
+
+
+class Tester : public Employee
+{
+private:
+	string m_Category;
+public:
+	Tester() {};
+
+	Tester(int id, string name)
+		: Employee{id, name}
+	{}
+
+	void show() override;
+	
+	void input() override;
+};
+
